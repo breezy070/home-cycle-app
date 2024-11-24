bce6eb7e3c3f9d21a9497902e531fef522cbea01
Fabrizio Di Marco <fabrizio.dimarco@gmail.com>
2024-11-24 15:44:53 +0100

    testing api routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoute.js';
 
 dotenv.config();
 
@@ -10,10 +11,10 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log(err);
 });
 
-
-
 const app = express();
 
 app.listen(3000, ()=> {
     console.log('Server listening on port 3000');
 })
+
+app.use("/backend/user", userRoutes);
