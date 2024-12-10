c149a9da54ee843e529d187aebf6b0182d3a2f13
Fabrizio Di Marco <fabrizio.dimarco@gmail.com>
2024-12-11 00:07:22 +0100

    added map fontawesome icon for assigning zones link

--- client/src/components/profileDropDown.jsx
+++ client/src/components/profileDropDown.jsx
@@ -61,49 +61,64 @@ const ProfileDropdown = (props) => {
           <ul className="flex flex-col py-2">
             <Link onClick={handleCloseDropdown} to='/profile' className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                 <div className="flex flex-row justify-start items-baseline gap-x-2 w-full ">
-                    <div className="fa-solid fa-user w-5 h-5"></div>
-                    <div>Profile</div>
+                  <div className="fa-solid fa-user w-5 h-5"></div>
+                  <div>Profile</div>
                 </div>
             </Link>
             {currentUser?.role === "admin"
               ?
                 <Link onClick={handleCloseDropdown} to='/technician-zone-assignment' className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                   <div className="flex flex-row justify-start items-baseline gap-x-2 w-full ">
-                      <div className="fa-solid fa-user w-5 h-5"></div>
-                      <div>Assign Zone</div>
+                    <div className="fa-solid fa-map w-5 h-5"></div>
+                    <div>Assign Zone</div>
                   </div>
                 </Link>
               :
               ''
             }
-            <Link onClick={handleCloseDropdown} to='/achats' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-            <div className="flex flex-row justify-start items-baseline gap-x-2 w-full ">
-                <span className="fa-solid fa-cart-shopping w-5 h-5"></span>
-                <span>Achats</span>
-            </div>
-            </Link>
+            {currentUser?.role === "user"
+              ?
+              <Link onClick={handleCloseDropdown} to='/achats' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                <div className="flex flex-row justify-start items-baseline gap-x-2 w-full ">
+                  <span className="fa-solid fa-cart-shopping w-5 h-5"></span>
+                  <span>Achats</span>
+                </div>
+              </Link>
+              :
+              ''
+            }
             <Link onClick={handleCloseDropdown} to='/interventions' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
             <div className="flex flex-row justify-start items-baseline gap-x-2 w-full ">
                 <span className="fa-solid fa-calendar-check w-5 h-5"></span>
                 <span>Interventions</span>
             </div>
             </Link>
+            {currentUser?.role === "user"
+              ?
             <Link onClick={handleCloseDropdown} to='/factures' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-            <div className="flex flex-row justify-start items-baseline gap-x-2 w-full ">
+              <div className="flex flex-row justify-start items-baseline gap-x-2 w-full ">
                 <span className="fa-solid fa-receipt w-5 h-5"></span>
                 <span>Factures</span>
-            </div>
+              </div>
             </Link>
+              :
+              ''
+            }
+            {currentUser?.role === "user"
+              ?
             <Link onClick={handleCloseDropdown} to='/velos' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-            <div className="flex flex-row justify-start items-baseline gap-x-2 w-full ">
+              <div className="flex flex-row justify-start items-baseline gap-x-2 w-full ">
                 <span className="fa-solid fa-bicycle w-5 h-5"></span>
                 <span>Vélos</span>
-            </div>
+              </div>
             </Link>
+              :
+              ''
+            }
             <Link onClick={handleSignOut} className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100">
             <div className="flex flex-row justify-start items-baseline gap-x-2 w-full ">
-                <span className="fa-solid fa-right-from-bracket w-5 h-5"></span>
-                <span>Sign Out</span>
+              <span className="fa-solid fa-right-from-bracket w-5 h-5"></span>
+              <span>Sign Out</span>
             </div>
             </Link>
             {/* <li className="bg-red-50 rounded-sm" onClick={}>
