4235e4b86e096afc17feff300edcc936920c7f20
Fabrizio Di Marco <fabrizio.dimarco@gmail.com>
2024-12-12 12:59:49 +0100

    making the interventions page more dynamic : if user then show user interventions ; if technician: show technician interventions. also adjusted colors and UI

--- client/src/pages/Interventions.jsx
+++ client/src/pages/Interventions.jsx
@@ -8,13 +8,28 @@ export default function Interventions() {
   const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
+    console.log(currentUser)
     const fetchAppointments = async () => {
-      try {
-        const response = await axios.get(`/api/user/scheduled-appointments/${currentUser._id}`);
-        console.log(response.data.appointments);
-        setAppointments(response.data.appointments);
-      } catch (err) {
-        console.error(err);
+      if (currentUser.role === 'user') {
+        console.log("displaying user appointments")
+        try {
+          const response = await axios.get(`/api/user/scheduled-appointments/${currentUser._id}`);
+          console.log(response.data);
+          setAppointments(response.data.appointments);
+        } catch (err) {
+          console.error(err);
+        }
+      } else if (currentUser.role === 'technician') {
+        console.log("displaying technician appointments")
+        try {
+          const response = await axios.get(`/api/technician/scheduled-technician-appointments/${currentUser._id}`);
+          console.log(response.data);
+          setAppointments(response.data.appointments);
+        } catch (err) {
+          console.error(err);
+        }
+      } else {
+        console.log("displaying admin")
       }
     };
 
@@ -46,6 +61,54 @@ export default function Interventions() {
       // Optionally, handle errors (e.g., show an error message)
     }
   };
+
+  const handleAcceptAppointment = async (appointmentId) => {
+    try {
+      // Make an API call to update the status of the appointment
+      const response = await axios.put(`/api/user/scheduled-appointments/update/${appointmentId}`, {
+        status: 'Accepted', // or pass this as part of the request body
+      });
+  
+      if (response.status === 200) {
+        // Update the status in the local state
+        setAppointments((prev) =>
+          prev.map((appointment) =>
+            //important to use the mongo ._id
+            appointment._id === appointmentId
+              ? { ...appointment, status: 'Accepted' }
+              : appointment
+          )
+        );
+      }
+    } catch (error) {
+      console.error('Failed to refuse appointment:', error);
+      // Optionally, handle errors (e.g., show an error message)
+    }
+  };
+
+  const handleRefuseAppointment = async (appointmentId) => {
+    try {
+      // Make an API call to update the status of the appointment
+      const response = await axios.put(`/api/user/scheduled-appointments/update/${appointmentId}`, {
+        status: 'Refused', // or pass this as part of the request body
+      });
+  
+      if (response.status === 200) {
+        // Update the status in the local state
+        setAppointments((prev) =>
+          prev.map((appointment) =>
+            //important to use the mongo ._id
+            appointment._id === appointmentId
+              ? { ...appointment, status: 'Refused' }
+              : appointment
+          )
+        );
+      }
+    } catch (error) {
+      console.error('Failed to refuse appointment:', error);
+      // Optionally, handle errors (e.g., show an error message)
+    }
+  };
   
   useEffect(() => {
     console.log('Updated Appointments:', appointments);
@@ -63,32 +126,101 @@ export default function Interventions() {
         <div className='flex flex-col gap-10 p-10'>
           {appointments.map((appointment) => (
             <div key={appointment._id} className={`flex flex-row gap-10 w-full ${appointment.status === 'Cancelled' ? `bg-slate-700 opacity-50` : `bg-slate-200`} rounded-xl p-5`}>
+              {/* PROFILE IMAGES */}
+              {currentUser.role === 'technician'
+              ? 
               <img 
-                src={appointment.technicianId?.profilePicture || 'default-technician.jpg'} 
-                alt={appointment.technicianId?.first_name || 'Technician'} 
+                src={appointment.userId?.profilePicture || 'default-technician.jpg'} 
+                alt={appointment.userId?.first_name || 'Technician'} 
                 className='object-cover max-w-44 max-h-44 rounded-xl'
               />
+              :
+              ''
+              }
+
+              {currentUser.role === 'user'
+              ? <img 
+                src={appointment.technicianId?.profilePicture || 'default-user.jpg'} 
+                alt={appointment.technicianId?.first_name || 'User'} 
+                className='object-cover max-w-44 max-h-44 rounded-xl'
+                />
+              :
+              ''
+              }
+
+              {/* APPOINTMENT INFORMATION */}
+
+              {currentUser.role === 'technician'
+              ? 
+              <div className= 'flex flex-col justify-between w-full'> 
+                <h3 className='text-2xl'>{appointment.userId?.first_name || 'Technician Name'}</h3>
+                <p>Date: {new Date(appointment.date).toLocaleString('fr-FR')}</p>
+
+                {appointment.status === 'Cancelled' || appointment.status === 'Refused'
+                ? <p className={`text-red-500`}>Status: {appointment.status}</p>
+                :''
+                }
+                {appointment.status === 'Pending' || appointment.status === 'In Progress'
+                ? <p className={`text-yellow-400`}>Status: {appointment.status}</p>
+                : ''
+                }
+                {appointment.status === 'Accepted'
+                ? <p className={`text-green-400`}>Status: {appointment.status}</p>
+                : ''
+                }
+                <p>Détails: {appointment.services.join(', ')}</p>
+              </div>
+              :
+              ''
+              }
+              
+              {currentUser.role === 'user'
+              ? 
               <div className= 'flex flex-col justify-between w-full'> 
                 <h3 className='text-2xl'>{appointment.technicianId?.first_name || 'Technician Name'}</h3>
                 <p>Date: {new Date(appointment.date).toLocaleString('fr-FR')}</p>
-                <p className={`${appointment.status === 'Cancelled' ? `text-red-500` : `text-yellow-400`}`}>Status: {appointment.status}</p>
+                {appointment.status === 'Cancelled' || appointment.status === 'Refused'
+                ? <p className={`text-red-500`}>Status: {appointment.status}</p>
+                :''
+                }
+                {appointment.status === 'Pending' || appointment.status === 'In Progress'
+                ? <p className={`text-yellow-400`}>Status: {appointment.status}</p>
+                : ''
+                }
+                {appointment.status === 'Accepted'
+                ? <p className={`text-green-400`}>Status: {appointment.status}</p>
+                : ''
+                }
                 <p>Détails: {appointment.services.join(', ')}</p>
               </div>
+              :
+              ''
+              }
+
+              {/* BUTTONS */}
+              {currentUser.role === 'technician'
+              ?
+                <div className="flex w-full justify-end">
+                  <div className="flex flex-col justify-end gap-3">
+                    <button onClick={() => handleAcceptAppointment(appointment._id)} className={`bg-green-700 text-white p-3 rounded-lg uppercase hover:opacity-95 w-60 h-12`} disabled={appointment.status === 'Cancelled'}>Accept</button>
+                    <button onClick={() => handleRefuseAppointment(appointment._id)} className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95 w-60 h-12' disabled={appointment.status === 'Cancelled'}>{appointment.status === 'Cancelled' ?" Appointment Cancelled" : "Refuse"}</button>
+                  </div>
+                </div>
+              :
+                ''
+              }
+              {currentUser.role === 'user'
+              ?
               <div className="flex w-full justify-end">
                 <div className="flex flex-col justify-end gap-3">
                   <button className={`bg-green-700 text-white p-3 rounded-lg uppercase hover:opacity-95 w-60 h-12`} disabled={appointment.status === 'Cancelled'}>Modifier</button>
                   <button className='bg-green-700 text-white p-3 rounded-lg uppercase hover:opacity-95 w-60 h-12' disabled={appointment.status === 'Cancelled'}>Consulter Facture</button>
                   <button onClick={() => handleCancelAppointment(appointment._id)} className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95 w-60 h-12' disabled={appointment.status === 'Cancelled'}>{appointment.status === 'Cancelled' ?" Appointment Cancelled" : "Cancel Appointment"}</button>
-                  {/* {appointment.status !== 'cancelled' && (
-                  <button
-                    onClick={() => handleCancelAppointment(appointment.id)}
-                    className="cancel-button bg-red-600 text-white p-2 rounded-md"
-                  >
-                    Cancel Appointment
-                  </button>
-                  )} */}
                 </div>
               </div>
+              :
+                ''
+              }
             </div>
           ))}
         </div>
