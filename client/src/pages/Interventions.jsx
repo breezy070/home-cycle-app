9f03084de357e0d8b58d3221e93ee97423d7ff4e
Fabrizio Di Marco <fabrizio.dimarco@gmail.com>
2025-08-18 20:56:14 +0200

    testing secure:true

--- client/src/pages/Interventions.jsx
+++ client/src/pages/Interventions.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-// import api from '@/api/axiosInstance';
-import axios, { all } from 'axios';
+import api from '@/api/axiosInstance';
+
 
 import { useSelector } from 'react-redux';
 
@@ -21,7 +21,7 @@ export default function Interventions() {
       if (currentUser.role === 'user') {
         console.log("displaying user appointments")
         try {
-          const response = await axios.get(`/api/user/scheduled-appointments/${currentUser._id}`);
+          const response = await api.get(`/api/user/scheduled-appointments/${currentUser._id}`);
           console.log(response.data);
           setAppointments(response.data.appointments);
           setFilteredAppointments(response.data.appointments);
